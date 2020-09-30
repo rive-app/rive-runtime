26ce6a3396381c34258c7622f083d6a310056b35
Luigi Rosso <luigi.rosso@gmail.com>
2020-09-30 12:13:45 -0700

    Fixing mixing of floating point keyframes.

--- src/animation/keyframe_double.cpp
+++ src/animation/keyframe_double.cpp
@@ -3,9 +3,30 @@
 
 using namespace rive;
 
+// This whole class is intentionally misnamed to match our editor code. The
+// editor uses doubles (float64) for numeric values but at runtime 32 bit
+// floating point numbers suffice. So even though this is a "double keyframe" to
+// match editor names, the actual values are stored and applied in 32 bits.
+
+static void applyDouble(Core* object, int propertyKey, float mix, float value)
+{
+	if (mix == 1.0f)
+	{
+		CoreRegistry::setDouble(object, propertyKey, value);
+	}
+	else
+	{
+		float mixi = 1.0 - mix;
+		CoreRegistry::setDouble(
+		    object,
+		    propertyKey,
+		    CoreRegistry::getDouble(object, propertyKey) * mixi + value * mix);
+	}
+}
+
 void KeyFrameDouble::apply(Core* object, int propertyKey, float mix)
 {
-	CoreRegistry::setDouble(object, propertyKey, value() * mix);
+	applyDouble(object, propertyKey, mix, value());
 }
 
 void KeyFrameDouble::applyInterpolation(Core* object,
@@ -14,8 +35,8 @@ void KeyFrameDouble::applyInterpolation(Core* object,
                                         const KeyFrame* nextFrame,
                                         float mix)
 {
-    auto kfd = nextFrame->as<KeyFrameDouble>();
-    const KeyFrameDouble& nextDouble = *kfd;
+	auto kfd = nextFrame->as<KeyFrameDouble>();
+	const KeyFrameDouble& nextDouble = *kfd;
 	float f = (currentTime - seconds()) / (nextDouble.seconds() - seconds());
 
 	if (CubicInterpolator* cubic = interpolator())
@@ -23,19 +44,6 @@ void KeyFrameDouble::applyInterpolation(Core* object,
 		f = cubic->transform(f);
 	}
 
-	float interpolatedValue = value() + (nextDouble.value() - value()) * f;
-
-	if (mix == 1.0f)
-	{
-		CoreRegistry::setDouble(object, propertyKey, interpolatedValue);
-	}
-	else
-	{
-		float mixi = 1.0 - mix;
-		CoreRegistry::setDouble(object,
-		                        propertyKey,
-		                        CoreRegistry::getDouble(object, propertyKey) *
-		                                mixi +
-		                            interpolatedValue * mix);
-	}
+	applyDouble(
+	    object, propertyKey, mix, value() + (nextDouble.value() - value()) * f);
 }
