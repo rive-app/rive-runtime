cfcbaac302988753e5af967ff37013f1048f6af3
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-02-22 13:55:42 +0000

    updated gitignore to include ffmpeg and h264

--- skia/recorder/build/premake5.lua
+++ skia/recorder/build/premake5.lua
@@ -1,5 +1,13 @@
-workspace "rive_recorder"
+workspace "rive"
 configurations {"debug", "release"}
+
+BASE_DIR = path.getabsolute("../../../build")
+location("./")
+dofile(path.join(BASE_DIR, "premake5.lua"))
+
+BASE_DIR = path.getabsolute("../../renderer/build")
+location("./")
+dofile(path.join(BASE_DIR, "premake5.lua"))
 
 project "rive_recorder"
 kind "ConsoleApp"
@@ -7,23 +15,62 @@ language "C++"
 cppdialect "C++17"
 targetdir "bin/%{cfg.buildcfg}"
 objdir "obj/%{cfg.buildcfg}"
-includedirs {"../../../include", "../../renderer/include", "../../dependencies/skia",
-             "../../dependencies/skia/include/core", "../../dependencies/skia/include/effects",
-             "../../dependencies/skia/include/gpu", "../../dependencies/skia/include/config"}
+includedirs {"../include", "../../../include", "../../renderer/include", "../../dependencies/glfw/include",
+             "../../dependencies/skia", "../../dependencies/skia/include/core",
+             "../../dependencies/skia/include/effects", "../../dependencies/skia/include/gpu",
+             "../../dependencies/skia/include/config", 
+             "../../dependencies/FFmpeg", 
+             "../../dependencies/x264/include", 
+             "/usr/local/include",
+            }
 
-if os.host() == "macosx" then
-    links {"Cocoa.framework",  "rive", "skia", "rive_skia_renderer"}
-else 
-    links {"rive", "skia", "rive_skia_renderer"}
-end
-links {"avcodec", "avformat", "avutil", "swscale"}
 
-libdirs {"../../../build/bin/%{cfg.buildcfg}", "../../dependencies/skia/out/Static",
-         "../../renderer/build/bin/%{cfg.buildcfg}"}
+links {
+    "avcodec", 
+    "avformat", 
+    "avutil", 
+    "glfw3", 
+    "z",
+    "bz2",
+    "iconv",
+    "lzma",
+    "m",
+    "rive_skia_renderer", 
+    "rive", 
+    "skia", 
+    "swscale", 
+    "swresample", 
+    "AudioToolbox.framework", 
+    "AudioUnit.framework", 
+    "Cocoa.framework", 
+    "CoreFoundation.framework", 
+    "CoreServices.framework", 
+    "CoreVideo.framework", 
+    "CoreMedia.framework",
+    "Security.framework",
+    "IOKit.framework", 
+    "VideoToolbox.framework",
+    "x264",
+}
+
+libdirs {
+    "../../../build/bin/%{cfg.buildcfg}", 
+    "../../dependencies/FFmpeg/libavcodec", 
+    "../../dependencies/FFmpeg/libavformat", 
+    "../../dependencies/FFmpeg/libavutil", 
+    "../../dependencies/FFmpeg/libswscale",
+    "../../dependencies/FFmpeg/libswresample",
+    "../../dependencies/x264/lib",
+    "../../dependencies/glfw_build/src",
+    "../../dependencies/skia/out/Static", 
+    
+    "../../renderer/build/bin/%{cfg.buildcfg}", 
+    "/usr/local/lib",
+}
 
 files {"../src/**.cpp"}
 
-buildoptions {"-Wall"}
+buildoptions {"-Wall", "-fno-rtti"}
 
 filter "configurations:debug"
 defines {"DEBUG"}
@@ -31,6 +78,7 @@ symbols "On"
 
 filter "configurations:release"
 defines {"RELEASE"}
+defines { "NDEBUG" }
 optimize "On"
 
 -- Clean Function --
