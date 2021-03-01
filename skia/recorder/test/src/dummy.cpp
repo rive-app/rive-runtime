2a5a4602ecfb71fc5592239a9a5e4e8675ea438e
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-03-01 14:37:49 +0000

    fix up tests to at least run

--- skia/recorder/test/src/dummy.cpp
+++ skia/recorder/test/src/dummy.cpp
@@ -1,4 +1,3 @@
 #include "catch.hpp"
 
 TEST_CASE("Dummy pass") { REQUIRE(0.0f == 0.0f); }
-// TEST_CASE("Dummy fail") { REQUIRE(0.0f == 0.2f); }
--- skia/recorder/test/src/stringFormat.cpp
+++ skia/recorder/test/src/stringFormat.cpp
@@ -1,25 +1,18 @@
 #include "catch.hpp"
-#include "main.hpp"
+#include "util.hxx"
 
 TEST_CASE("String format check hello world")
 {
 	REQUIRE(string_format("hello %s", "world") == "hello world");
 }
 
-TEST_CASE("String format check hello world")
+TEST_CASE("String format check hello cruel world")
 {
-	REQUIRE(string_format("hello %s %d", "cruel", "world") == "hello world 1");
+	REQUIRE(string_format("hello %s %s", "cruel", "world") ==
+	        "hello cruel world");
 }
 
-// Was kinda expecting to be able to use this in multiple ways. but i think i
-// need some different compiler thign for hat..
-//
-// Undefined symbols for architecture x86_64:
-//   "std::__1::basic_string<char, std::__1::char_traits<char>,
-//   std::__1::allocator<char> > string_format<char const*,
-//   int>(std::__1::basic_string<char, std::__1::char_traits<char>,
-//   std::__1::allocator<char> > const&, char const*, int)", referenced from:
-//       ____C_A_T_C_H____T_E_S_T____2() in stringFormat.o
-// ld: symbol(s) not found for architecture x86_64
-// REQUIRE(string_format("hello %s %d", "cruel", "world") == "hello world 1");
-// REQUIRE(string_format("hello {} {}", "world", 2) == "hello world 2");
+TEST_CASE("String format check hello sweet world")
+{
+	REQUIRE(string_format("hello %s %d", "sweet", 1) == "hello sweet 1");
+}
