rive-app/rive-runtime

commit 8b317d700bdc65c2378ffbcd00e869f2887ecf5d

Support for Windows (#41)
* Add `_USE_MATH_DEFINES` on windows builds

* Use an `std::vector<char>` instead of a variable-length char array

src/core/binary_reader.cpp
@@ -44,7 +44,7 @@ std::string BinaryReader::readString()
 		return std::string();
 	}
 
-	char rawValue[length + 1];
+	std::vector<char> rawValue(length + 1);
 	auto readBytes = decode_string(length, m_Position, m_End, &rawValue[0]);
 	if (readBytes != length)
 	{
@@ -52,7 +52,7 @@ std::string BinaryReader::readString()
 		return std::string();
 	}
 	m_Position += readBytes;
-	return std::string(rawValue);
+	return std::string(rawValue.data(), rawValue.size());
 }
 
 double BinaryReader::readFloat64()

build/premake5.lua
@@ -1,26 +1,29 @@
 workspace "rive"
-configurations {"debug", "release"}
+    configurations {"debug", "release"}
 
 project "rive"
-kind "StaticLib"
-language "C++"
-cppdialect "C++17"
-targetdir "bin/%{cfg.buildcfg}"
-objdir "obj/%{cfg.buildcfg}"
-includedirs {"../include"}
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+    includedirs {"../include"}
 
-files {"../src/**.cpp"}
+    files {"../src/**.cpp"}
 
-buildoptions {"-Wall", "-fno-exceptions", "-fno-rtti"}
+    buildoptions {"-Wall", "-fno-exceptions", "-fno-rtti"}
 
-filter "configurations:debug"
-defines {"DEBUG"}
-symbols "On"
+    filter "system:windows"
+        defines {"_USE_MATH_DEFINES"}
 
-filter "configurations:release"
-defines {"RELEASE"}
-defines { "NDEBUG" }
-optimize "On"
+    filter "configurations:debug"
+        defines {"DEBUG"}
+        symbols "On"
+
+    filter "configurations:release"
+        defines {"RELEASE"}
+        defines {"NDEBUG"}
+        optimize "On"
 
 -- Clean Function --
 newaction {

commit 3cb2a0fb33c104160086830fb4f17ef9361067e6

Fixing readString issue introduced by #41.

src/core/binary_reader.cpp
@@ -1,5 +1,6 @@
 #include "core/binary_reader.hpp"
 #include "core/reader.h"
+#include <vector>
 
 using namespace rive;
 
@@ -11,7 +12,10 @@ BinaryReader::BinaryReader(uint8_t* bytes, size_t length) :
 {
 }
 
-bool BinaryReader::reachedEnd() const { return m_Position == m_End || didOverflow(); }
+bool BinaryReader::reachedEnd() const
+{
+	return m_Position == m_End || didOverflow();
+}
 
 size_t BinaryReader::lengthInBytes() const { return m_Length; }
 
@@ -52,7 +56,7 @@ std::string BinaryReader::readString()
 		return std::string();
 	}
 	m_Position += readBytes;
-	return std::string(rawValue.data(), rawValue.size());
+	return std::string(rawValue.data(), length);
 }
 
 double BinaryReader::readFloat64()