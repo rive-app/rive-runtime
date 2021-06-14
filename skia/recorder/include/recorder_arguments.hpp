a3d6b9de542abae1b9e268e83fbad460dec94ac2
Umberto Sonnino <umberto@rive.app>
2021-06-14 12:02:33 +0100

    Move args into their own class

---
+++ skia/recorder/include/recorder_arguments.hpp
@@ -0,0 +1,177 @@
+#include "args.hxx"
+#include <iostream>
+
+class RecorderArguments
+{
+
+public:
+	RecorderArguments(int argc, char** argv)
+	{
+		// PARSE INPUT
+		m_Parser = new args::ArgumentParser(
+		    "Record playback of a Rive file as a movie, gif, etc (eventually "
+		    "should support image sequences saved in a zip/archive too).",
+		    "Experimental....");
+		args::HelpFlag help(
+		    *m_Parser, "help", "Display this help menu", {'h', "help"});
+		args::Group required(
+		    *m_Parser, "required arguments:", args::Group::Validators::All);
+		args::Group optional(*m_Parser,
+		                     "optional arguments:",
+		                     args::Group::Validators::DontCare);
+
+		args::ValueFlag<std::string> source(
+		    required, "path", "source filename", {'s', "source"});
+		args::ValueFlag<std::string> destination(
+		    required, "path", "destination filename", {'d', "destination"});
+		args::ValueFlag<std::string> animation(
+		    optional,
+		    "name",
+		    "animation to be played, determines the numbers of frames recorded",
+		    {'a', "animation"});
+		args::ValueFlag<std::string> artboard(
+		    optional, "name", "artboard to draw from", {'t', "artboard"});
+		args::ValueFlag<std::string> watermark(
+		    optional, "path", "watermark filename", {'w', "watermark"});
+
+		args::ValueFlag<int> width(
+		    optional, "number", "container width", {'W', "width"}, 0);
+
+		args::ValueFlag<int> height(
+		    optional, "number", "container height", {'H', "height"}, 0);
+
+		args::ValueFlag<int> smallExtentTarget(
+		    optional,
+		    "number",
+		    "target size for smaller edge of container",
+		    {"small-extent-target"},
+		    0);
+
+		args::ValueFlag<int> maxWidth(
+		    optional, "number", "maximum container width", {"max-width"}, 0);
+
+		args::ValueFlag<int> maxHeight(
+		    optional, "number", "maximum container height", {"max-height"}, 0);
+
+		args::ValueFlag<int> minDuration(optional,
+		                                 "number",
+		                                 "minimum duration in seconds",
+		                                 {"min-duration"},
+		                                 0);
+
+		args::ValueFlag<int> maxDuration(optional,
+		                                 "number",
+		                                 "maximum duration in seconds",
+		                                 {"max-duration"},
+		                                 0);
+
+		args::ValueFlag<int> numLoops(
+		    optional,
+		    "number",
+		    "number of times this video should be looped for",
+		    {"num-loops"},
+		    1);
+
+		// 0 will use VBR. By setting a bitrate value, users enforce CBR.
+		args::ValueFlag<int> bitrate(
+		    optional, "number", "bitrate in kbps", {"bitrate"}, 0);
+
+		args::ValueFlag<std::string> snapshotPath(
+		    optional, "path", "destination image filename", {"snapshot-path"});
+
+		args::ValueFlag<float> fps(
+		    required, "number", "frame rate", {"f", "fps"}, 60.0);
+
+		args::CompletionFlag completion(*m_Parser, {"complete"});
+		try
+		{
+			m_Parser->ParseCLI(argc, argv);
+		}
+		catch (const std::invalid_argument e)
+		{
+			std::cout << e.what();
+			throw;
+		}
+		catch (const args::Completion& e)
+		{
+			std::cout << e.what();
+			throw;
+		}
+		catch (const args::Help&)
+		{
+			std::cout << m_Parser;
+			throw;
+		}
+		catch (const args::ParseError& e)
+		{
+			std::cerr << e.what() << std::endl;
+			std::cerr << m_Parser;
+			throw;
+		}
+		catch (args::ValidationError e)
+		{
+			std::cerr << e.what() << std::endl;
+			std::cerr << m_Parser;
+			throw;
+		}
+
+		m_Source = args::get(source);
+		m_Destination = args::get(destination);
+		m_Animation = args::get(animation);
+		m_Artboard = args::get(artboard);
+		m_Watermark = args::get(watermark);
+		m_SnapshotPath = args::get(snapshotPath);
+		m_Width = args::get(width);
+		m_Height = args::get(height);
+		m_MaxWidth = args::get(maxWidth);
+		m_MaxHeight = args::get(maxHeight);
+		m_SmallExtentTarget = args::get(smallExtentTarget);
+		m_MinDuration = args::get(minDuration);
+		m_MaxDuration = args::get(maxDuration);
+		m_NumLoops = args::get(numLoops);
+		m_Bitrate = args::get(bitrate);
+		m_Fps = args::get(fps);
+	}
+
+	~RecorderArguments()
+	{
+		if (m_Parser != nullptr)
+			delete m_Parser;
+	}
+
+	std::string source() const { return m_Source; }
+	std::string destination() const { return m_Destination; }
+	std::string animation() const { return m_Animation; }
+	std::string artboard() const { return m_Artboard; }
+	std::string watermark() const { return m_Watermark; }
+	std::string snapshotPath() const { return m_SnapshotPath; }
+	int width() const { return m_Width; }
+	int height() const { return m_Height; }
+	int maxWidth() const { return m_MaxWidth; }
+	int maxHeight() const { return m_MaxHeight; }
+	int smallExtentTarget() const { return m_SmallExtentTarget; }
+	int minDuration() const { return m_MinDuration; }
+	int maxDuration() const { return m_MaxDuration; }
+	int numLoops() const { return m_NumLoops; }
+	int bitrate() const { return m_Bitrate; }
+	float fps() const { return m_Fps; }
+
+private:
+	args::ArgumentParser* m_Parser;
+	std::string m_Source;
+	std::string m_Destination;
+	std::string m_Animation;
+	std::string m_Artboard;
+	std::string m_Watermark;
+	std::string m_SnapshotPath;
+	int m_Width;
+	int m_Height;
+	int m_MaxWidth;
+	int m_MaxHeight;
+	int m_SmallExtentTarget;
+	int m_MinDuration;
+	int m_MaxDuration;
+	int m_NumLoops;
+	int m_Bitrate;
+	float m_Fps;
+};
--- skia/recorder/src/main.cpp
+++ skia/recorder/src/main.cpp
@@ -1,6 +1,7 @@
 #include "args.hxx"
 #include "extractor.hpp"
 #include "writer.hpp"
+#include "recorder_arguments.hpp"
 
 extern "C"
 {
@@ -30,128 +31,43 @@ extern "C"
 #else
 int main(int argc, char* argv[])
 {
-	// PARSE INPUT
-	args::ArgumentParser parser(
-	    "Record playback of a Rive file as a movie, gif, etc (eventually "
-	    "should support image sequences saved in a zip/archive too).",
-	    "Experimental....");
-	args::HelpFlag help(
-	    parser, "help", "Display this help menu", {'h', "help"});
-	args::Group required(
-	    parser, "required arguments:", args::Group::Validators::All);
-	args::Group optional(
-	    parser, "optional arguments:", args::Group::Validators::DontCare);
-
-	args::ValueFlag<std::string> source(
-	    required, "path", "source filename", {'s', "source"});
-	args::ValueFlag<std::string> destination(
-	    required, "path", "destination filename", {'d', "destination"});
-	args::ValueFlag<std::string> animationOption(
-	    optional,
-	    "name",
-	    "animation to be played, determines the numbers of frames recorded",
-	    {'a', "animation"});
-	args::ValueFlag<std::string> artboardOption(
-	    optional, "name", "artboard to draw from", {'t', "artboard"});
-	args::ValueFlag<std::string> watermarkOption(
-	    optional, "path", "watermark filename", {'w', "watermark"});
-
-	args::ValueFlag<int> width(
-	    optional, "number", "container width", {'W', "width"}, 0);
-
-	args::ValueFlag<int> height(
-	    optional, "number", "container height", {'H', "height"}, 0);
-
-	args::ValueFlag<int> small_extent_target(
-	    optional,
-	    "number",
-	    "target size for smaller edge of container",
-	    {"small-extent-target"},
-	    0);
-
-	args::ValueFlag<int> max_width(
-	    optional, "number", "maximum container width", {"max-width"}, 0);
-
-	args::ValueFlag<int> max_height(
-	    optional, "number", "maximum container height", {"max-height"}, 0);
-
-	args::ValueFlag<int> min_duration(
-	    optional, "number", "minimum duration in seconds", {"min-duration"}, 0);
-
-	args::ValueFlag<int> max_duration(
-	    optional, "number", "maximum duration in seconds", {"max-duration"}, 0);
-
-	args::ValueFlag<int> num_loops(
-	    optional,
-	    "number",
-	    "number of times this video should be looped for",
-	    {"num-loops"},
-	    1);
-
-	// 0 will use VBR. By setting a bitrate value, users enforce CBR.
-	args::ValueFlag<int> bitrate(
-	    optional, "number", "bitrate in kbps", {"bitrate"}, 0);
-
-	args::ValueFlag<std::string> snapshot_path(
-	    optional, "path", "destination image filename", {"snapshot-path"});
-
-	args::ValueFlag<float> fps(
-	    required, "number", "frame rate", {"f", "fps"}, 60.0);
-
-	args::CompletionFlag completion(parser, {"complete"});
-
 	try
 	{
-		parser.ParseCLI(argc, argv);
+		RecorderArguments args(argc, argv);
+		RiveFrameExtractor extractor(args.source().c_str(),
+		                             args.artboard().c_str(),
+		                             args.animation().c_str(),
+		                             args.watermark().c_str(),
+		                             args.width(),
+		                             args.height(),
+		                             args.smallExtentTarget(),
+		                             args.maxWidth(),
+		                             args.maxHeight(),
+		                             args.minDuration(),
+		                             args.maxDuration(),
+		                             args.fps());
+		MovieWriter writer(args.destination().c_str(),
+		                   extractor.width(),
+		                   extractor.height(),
+		                   extractor.fps(),
+		                   args.bitrate());
+		extractor.takeSnapshot(args.snapshotPath());
+		extractor.extractVideo(args.numLoops(), writer);
 	}
 	catch (const args::Completion& e)
 	{
-		std::cout << e.what();
 		return 0;
 	}
 	catch (const args::Help&)
 	{
-		std::cout << parser;
 		return 0;
 	}
 	catch (const args::ParseError& e)
 	{
-		std::cerr << e.what() << std::endl;
-		std::cerr << parser;
 		return 1;
 	}
 	catch (args::ValidationError e)
 	{
-		std::cerr << e.what() << std::endl;
-		std::cerr << parser;
-		return 1;
-	}
-
-	try
-	{
-		RiveFrameExtractor extractor(args::get(source).c_str(),
-		                             args::get(artboardOption).c_str(),
-		                             args::get(animationOption).c_str(),
-		                             args::get(watermarkOption).c_str(),
-		                             args::get(width),
-		                             args::get(height),
-		                             args::get(small_extent_target),
-		                             args::get(max_width),
-		                             args::get(max_height),
-		                             args::get(min_duration),
-		                             args::get(max_duration),
-		                             args::get(fps));
-		MovieWriter writer(args::get(destination).c_str(),
-		                   extractor.width(),
-		                   extractor.height(),
-		                   extractor.fps(),
-		                   args::get(bitrate));
-		extractor.takeSnapshot(args::get(snapshot_path));
-		extractor.extractVideo(args::get(num_loops), writer);
-	}
-	catch (const std::invalid_argument e)
-	{
-		std::cout << e.what();
 		return 1;
 	}
 }
