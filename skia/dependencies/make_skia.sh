497ef5dbd11427dd8305e3935352c62a4017dfab
Luigi Rosso <luigi.rosso@gmail.com>
2022-01-25 14:35:11 -0800

    Comments

--- skia/dependencies/make_skia.sh
+++ skia/dependencies/make_skia.sh
@@ -4,8 +4,8 @@ set -e
 
 ./get_skia.sh
 
+# use Rive optimized/stripped Skia for iOS static libs.
 cd skia_rive_optimized
-
 
 python tools/git-sync-deps
 bin/gn gen out/ios64 --type=static_library --args=" \
@@ -206,6 +206,7 @@ ninja -C out/iossim_arm64
 xcrun -sdk iphoneos lipo -create -arch armv7 out/ios32/libskia.a out/ios64/libskia.a -output out/libskia_ios.a
 xcrun -sdk iphoneos lipo -create -arch x86_64 out/iossim_x64/libskia.a -arch i386 out/iossim_x86/libskia.a out/iossim_arm64/libskia.a -output out/libskia_ios_sim.a
 
+# build regular/full skia
 cd ../skia
 
 # build static for host
