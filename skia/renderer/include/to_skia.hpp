062efcb1a10958a631e0efbba53b9d66beb0e114
Mike Reed <mike@reedtribe.org>
2022-04-02 11:49:50 -0400

    restore readable formatting

--- skia/renderer/include/to_skia.hpp
+++ skia/renderer/include/to_skia.hpp
@@ -19,16 +19,13 @@ namespace rive {
             return SkPoint::Make(point[0], point[1]);
         }
 
+        // clang-format off
         static SkTileMode convert(RenderTileMode rtm) {
             switch (rtm) {
-                case RenderTileMode::clamp:
-                    return SkTileMode::kClamp;
-                case RenderTileMode::repeat:
-                    return SkTileMode::kRepeat;
-                case RenderTileMode::mirror:
-                    return SkTileMode::kMirror;
-                case RenderTileMode::decal:
-                    return SkTileMode::kDecal;
+                case RenderTileMode::clamp:  return SkTileMode::kClamp;
+                case RenderTileMode::repeat: return SkTileMode::kRepeat;
+                case RenderTileMode::mirror: return SkTileMode::kMirror;
+                case RenderTileMode::decal:  return SkTileMode::kDecal;
             }
             assert(false);
             return SkTileMode::kClamp;
@@ -36,10 +33,8 @@ namespace rive {
 
         static SkPathFillType convert(FillRule value) {
             switch (value) {
-                case FillRule::evenOdd:
-                    return SkPathFillType::kEvenOdd;
-                case FillRule::nonZero:
-                    return SkPathFillType::kWinding;
+                case FillRule::evenOdd: return SkPathFillType::kEvenOdd;
+                case FillRule::nonZero: return SkPathFillType::kWinding;
             }
             assert(false);
             return SkPathFillType::kWinding;
@@ -47,65 +42,47 @@ namespace rive {
 
         static SkPaint::Cap convert(rive::StrokeCap cap) {
             switch (cap) {
-                case rive::StrokeCap::butt:
-                    return SkPaint::Cap::kButt_Cap;
-                case rive::StrokeCap::round:
-                    return SkPaint::Cap::kRound_Cap;
-                case rive::StrokeCap::square:
-                    return SkPaint::Cap::kSquare_Cap;
+                case StrokeCap::butt:   return SkPaint::Cap::kButt_Cap;
+                case StrokeCap::round:  return SkPaint::Cap::kRound_Cap;
+                case StrokeCap::square: return SkPaint::Cap::kSquare_Cap;
             }
+            assert(false);
             return SkPaint::Cap::kButt_Cap;
         }
 
-        static SkPaint::Join convert(rive::StrokeJoin join) {
+        static SkPaint::Join convert(StrokeJoin join) {
             switch (join) {
-                case rive::StrokeJoin::bevel:
-                    return SkPaint::Join::kBevel_Join;
-                case rive::StrokeJoin::round:
-                    return SkPaint::Join::kRound_Join;
-                case rive::StrokeJoin::miter:
-                    return SkPaint::Join::kMiter_Join;
+                case StrokeJoin::bevel: return SkPaint::Join::kBevel_Join;
+                case StrokeJoin::round: return SkPaint::Join::kRound_Join;
+                case StrokeJoin::miter: return SkPaint::Join::kMiter_Join;
             }
+            assert(false);
             return SkPaint::Join::kMiter_Join;
         }
 
-        static SkBlendMode convert(rive::BlendMode blendMode) {
+        static SkBlendMode convert(BlendMode blendMode) {
             switch (blendMode) {
-                case rive::BlendMode::srcOver:
-                    return SkBlendMode::kSrcOver;
-                case rive::BlendMode::screen:
-                    return SkBlendMode::kScreen;
-                case rive::BlendMode::overlay:
-                    return SkBlendMode::kOverlay;
-                case rive::BlendMode::darken:
-                    return SkBlendMode::kDarken;
-                case rive::BlendMode::lighten:
-                    return SkBlendMode::kLighten;
-                case rive::BlendMode::colorDodge:
-                    return SkBlendMode::kColorDodge;
-                case rive::BlendMode::colorBurn:
-                    return SkBlendMode::kColorBurn;
-                case rive::BlendMode::hardLight:
-                    return SkBlendMode::kHardLight;
-                case rive::BlendMode::softLight:
-                    return SkBlendMode::kSoftLight;
-                case rive::BlendMode::difference:
-                    return SkBlendMode::kDifference;
-                case rive::BlendMode::exclusion:
-                    return SkBlendMode::kExclusion;
-                case rive::BlendMode::multiply:
-                    return SkBlendMode::kMultiply;
-                case rive::BlendMode::hue:
-                    return SkBlendMode::kHue;
-                case rive::BlendMode::saturation:
-                    return SkBlendMode::kSaturation;
-                case rive::BlendMode::color:
-                    return SkBlendMode::kColor;
-                case rive::BlendMode::luminosity:
-                    return SkBlendMode::kLuminosity;
+                case BlendMode::srcOver:    return SkBlendMode::kSrcOver;
+                case BlendMode::screen:     return SkBlendMode::kScreen;
+                case BlendMode::overlay:    return SkBlendMode::kOverlay;
+                case BlendMode::darken:     return SkBlendMode::kDarken;
+                case BlendMode::lighten:    return SkBlendMode::kLighten;
+                case BlendMode::colorDodge: return SkBlendMode::kColorDodge;
+                case BlendMode::colorBurn:  return SkBlendMode::kColorBurn;
+                case BlendMode::hardLight:  return SkBlendMode::kHardLight;
+                case BlendMode::softLight:  return SkBlendMode::kSoftLight;
+                case BlendMode::difference: return SkBlendMode::kDifference;
+                case BlendMode::exclusion:  return SkBlendMode::kExclusion;
+                case BlendMode::multiply:   return SkBlendMode::kMultiply;
+                case BlendMode::hue:        return SkBlendMode::kHue;
+                case BlendMode::saturation: return SkBlendMode::kSaturation;
+                case BlendMode::color:      return SkBlendMode::kColor;
+                case BlendMode::luminosity: return SkBlendMode::kLuminosity;
             }
+            assert(false);
             return SkBlendMode::kSrcOver;
         }
+        // clang-format off
     };
 } // namespace rive
 #endif
