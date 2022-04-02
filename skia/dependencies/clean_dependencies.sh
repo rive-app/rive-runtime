6df27344e74f38977a8b00828bb2d0b4148b84a6
Luigi Rosso <luigi.rosso@gmail.com>
2022-04-02 16:14:05 -0700

    Slight improvement to build scripts to iterate faster (and ability to clean all dependencies).

---
+++ skia/dependencies/clean_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -fR ./gl3w
+rm -fR ./glfw
+rm -fR ./glfw_build
+rm -fR ./imgui
+rm -fR ./skia
+rm -fR ./skia_rive_optimized
--- skia/dependencies/make_skia.sh
+++ skia/dependencies/make_skia.sh
@@ -4,8 +4,9 @@ set -e
 
 ./get_skia.sh
 
+build_skia_rive_optimized() {
 # use Rive optimized/stripped Skia for iOS static libs.
-cd skia_rive_optimized
+pushd skia_rive_optimized
 
 python tools/git-sync-deps
 bin/gn gen out/ios64 --type=static_library --args=" \
@@ -205,11 +206,55 @@ ninja -C out/iossim_arm64
 xcrun -sdk iphoneos lipo -create -arch armv7 out/ios32/libskia.a out/ios64/libskia.a -output out/libskia_ios.a
 xcrun -sdk iphoneos lipo -create -arch x86_64 out/iossim_x64/libskia.a -arch i386 out/iossim_x86/libskia.a out/iossim_arm64/libskia.a -output out/libskia_ios_sim.a
 
+popd
+}
+
+build_skia() {
 # build regular/full skia
-cd ../skia
+pushd skia
+python tools/git-sync-deps
 
 # build static for host
-bin/gn gen out/static --type=static_library --args=" \
+bin/gn gen out/static/debug --type=static_library --args=" \
+    extra_cflags=[\"-fno-rtti\", \"-DSK_DISABLE_SKPICTURE\", \"-DSK_DISABLE_TEXT\", \"-DRIVE_OPTIMIZED\", \"-DSK_DISABLE_LEGACY_SHADERCONTEXT\", \"-DSK_DISABLE_LOWP_RASTER_PIPELINE\", \"-DSK_FORCE_RASTER_PIPELINE_BLITTER\", \"-DSK_DISABLE_AAA\", \"-DSK_DISABLE_EFFECT_DESERIALIZATION\"] \
+
+    is_official_build=false \
+    skia_use_gl=true \
+    skia_use_zlib=true \
+    skia_enable_gpu=true \
+    skia_enable_fontmgr_empty=false \
+    skia_use_libpng_encode=true \
+    skia_use_libpng_decode=true \
+    skia_enable_skgpu_v1=true \
+
+    skia_use_dng_sdk=false \
+    skia_use_egl=false \
+    skia_use_expat=false \
+    skia_use_fontconfig=false \
+    skia_use_freetype=false \
+    skia_use_icu=false \
+    skia_use_libheif=false \
+    skia_use_system_libpng=false \
+    skia_use_libjpeg_turbo_encode=false \
+    skia_use_libjpeg_turbo_decode=false \
+    skia_use_libwebp_encode=false \
+    skia_use_libwebp_decode=false \
+    skia_use_lua=false \
+    skia_use_piex=false \
+    skia_use_vulkan=false \
+    skia_use_metal=false \
+    skia_use_angle=false \
+    skia_use_system_zlib=false \
+    skia_enable_spirv_validation=false \
+    skia_enable_pdf=false \
+    skia_enable_skottie=false \
+    skia_enable_tools=false \
+    skia_enable_skgpu_v2=false \
+    "
+ninja -C out/static/debug
+du -hs out/static/debug/libskia.a
+
+bin/gn gen out/static/release --type=static_library --args=" \
     extra_cflags=[\"-fno-rtti\", \"-flto=full\", \"-DSK_DISABLE_SKPICTURE\", \"-DSK_DISABLE_TEXT\", \"-DRIVE_OPTIMIZED\", \"-DSK_DISABLE_LEGACY_SHADERCONTEXT\", \"-DSK_DISABLE_LOWP_RASTER_PIPELINE\", \"-DSK_FORCE_RASTER_PIPELINE_BLITTER\", \"-DSK_DISABLE_AAA\", \"-DSK_DISABLE_EFFECT_DESERIALIZATION\"] \
 
     is_official_build=true \
@@ -245,7 +290,10 @@ bin/gn gen out/static --type=static_library --args=" \
     skia_enable_tools=false \
     skia_enable_skgpu_v2=false \
     "
-ninja -C out/static
-du -hs out/static/libskia.a
+ninja -C out/static/release
+du -hs out/static/release/libskia.a
+popd
+}
 
-cd ..
+build_skia_rive_optimized
+build_skia
