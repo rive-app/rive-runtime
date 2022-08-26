81f77c894ecfff1eab8295814e0032b421dd0706
mikerreed <hello@rive.app>
2022-08-26 11:27:21 +0000

    add != to Mat2D
    fissioned from https://github.com/rive-app/rive/pull/4186
    
    Diffs=
    59ecc6712 Add != operator

--- include/rive/math/mat2d.hpp
+++ include/rive/math/mat2d.hpp
@@ -78,5 +78,8 @@ inline bool operator==(const Mat2D& a, const Mat2D& b) {
     return a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3] && a[4] == b[4] &&
            a[5] == b[5];
 }
+
+inline bool operator!=(const Mat2D& a, const Mat2D& b) { return !(a == b); }
+
 } // namespace rive
 #endif
