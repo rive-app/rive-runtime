71217cedd350895120068c8dfe4e797f3bef81ad
luigi-rosso <hello@rive.app>
2023-11-17 22:10:03 +0000

    Disable d3d blend state during PLS flush
    Diffs=
    fdad66136 Disable d3d blend state during PLS flush (#6254)
    
    Co-authored-by: Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
    Co-authored-by: Luigi Rosso <luigi.rosso@gmail.com>
    Co-authored-by: Maxwell Talbot <talbot.maxwell@gmail.com>

--- skia/dependencies/cache_helper.sh
+++ skia/dependencies/cache_helper.sh
@@ -14,8 +14,7 @@ OUTPUT_CACHE="${OUTPUT_CACHE:=out}"
 ARCHIVE_CONTENTS_NAME="${ARCHIVE_CONTENTS_NAME:=archive_contents}"
 
 # lets just make sure this exists, or fail
-if [[ ! -d $RIVE_RUNTIME_DIR ]]
-then
+if [[ ! -d $RIVE_RUNTIME_DIR ]]; then
     echo "Cannot find $RIVE_RUNTIME_DIR, bad setup"
     exit 1
 fi
@@ -28,7 +27,7 @@ SKIA_DIR="$SKIA_DEPENDENCIES_DIR/$SKIA_DIR_NAME"
 # this avoid issues with corrupted git repos throwing irrelevant errors
 pushd ~
 SKIA_COMMIT_HASH="$(git ls-remote $SKIA_REPO $SKIA_BRANCH | awk '{print $1}')"
-popd 
+popd
 
 ARCHIVE_CONTENTS_PATH="$SKIA_DIR/$ARCHIVE_CONTENTS_NAME"
 echo $ARCHIVE_CONTENTS_PATH
@@ -42,12 +41,12 @@ fi
 # TODO: hmm how do we know the make skia script.. i guess its an arg? a back arg?
 if [[ $OSTYPE == 'darwin'* ]]; then
     # md5 -r == md5sum
-    CONFIGURE_VERSION=$(md5 -r cache_helper.sh|awk '{print $1}')
-    MAKE_SKIA_HASH=$(md5 -r $SKIA_DEPENDENCIES_DIR/$MAKE_SKIA_FILE|awk '{print $1}')
+    CONFIGURE_VERSION=$(md5 -r cache_helper.sh | awk '{print $1}')
+    MAKE_SKIA_HASH=$(md5 -r $SKIA_DEPENDENCIES_DIR/$MAKE_SKIA_FILE | awk '{print $1}')
     BUILD_HASH=$(md5 -r -s "$SKIA_COMMIT_HASH $MAKE_SKIA_HASH $CONFIGURE_VERSION" | awk '{print $1}')
-else 
-    CONFIGURE_VERSION=$(md5sum cache_helper.sh|awk '{print $1}')
-    MAKE_SKIA_HASH=$(md5sum $SKIA_DEPENDENCIES_DIR/$MAKE_SKIA_FILE|awk '{print $1}')
+else
+    CONFIGURE_VERSION=$(md5sum cache_helper.sh | awk '{print $1}')
+    MAKE_SKIA_HASH=$(md5sum $SKIA_DEPENDENCIES_DIR/$MAKE_SKIA_FILE | awk '{print $1}')
     BUILD_HASH=$(echo "$SKIA_COMMIT_HASH $MAKE_SKIA_HASH $CONFIGURE_VERSION" | md5sum | awk '{print $1}')
 fi
 
@@ -56,44 +55,41 @@ echo "Created hash: $BUILD_HASH from skia_commit=$SKIA_COMMIT_HASH make_skia_scr
 EXPECTED_ARCHIVE_CONTENTS="$BUILD_HASH"_"$COMPILE_TARGET"
 
 ARCHIVE_FILE_NAME="$CACHE_NAME"_"$EXPECTED_ARCHIVE_CONTENTS.tar.gz"
-ARCHIVE_URL="https://cdn.2dimensions.com/archives/$ARCHIVE_FILE_NAME"
+ARCHIVE_URL="https://cdn.rive.app/archives/$ARCHIVE_FILE_NAME"
 ARCHIVE_PATH="$SKIA_DIR/$ARCHIVE_FILE_NAME"
 
 pull_cache() {
     echo "Grabbing cached build from $ARCHIVE_URL"
     mkdir -p $SKIA_DIR
-    curl --output $SKIA_DIR/$ARCHIVE_FILE_NAME $ARCHIVE_URL 
+    curl --output $SKIA_DIR/$ARCHIVE_FILE_NAME $ARCHIVE_URL
     pushd $SKIA_DIR
     tar -xf $ARCHIVE_FILE_NAME out include $ARCHIVE_CONTENTS_NAME third_party modules
 }
 
 is_build_cached_remotely() {
     echo "Checking for cache build $ARCHIVE_URL"
-    if curl --output /dev/null --head --silent --fail $ARCHIVE_URL
-    then 
+    if curl --output /dev/null --head --silent --fail $ARCHIVE_URL; then
         return 0
-    else 
+    else
         return 1
     fi
 }
 
 upload_cache() {
     pushd $SKIA_DEPENDENCIES_DIR
-    echo $EXPECTED_ARCHIVE_CONTENTS > $SKIA_DIR_NAME/$ARCHIVE_CONTENTS_NAME
+    echo $EXPECTED_ARCHIVE_CONTENTS >$SKIA_DIR_NAME/$ARCHIVE_CONTENTS_NAME
     # not really sure about this third party biz
     # also we are caching on a per architecture path here, but out could contain more :thinking:
     tar -C $SKIA_DIR_NAME -cf $SKIA_DIR_NAME/$ARCHIVE_FILE_NAME $OUTPUT_CACHE $ARCHIVE_CONTENTS_NAME include third_party/libpng third_party/externals/libpng modules
     popd
-    # # if we're configured to upload the archive back into our cache, lets do it! 
+    # # if we're configured to upload the archive back into our cache, lets do it!
     echo "Uploading to s3://2d-public/archives/$ARCHIVE_FILE_NAME"
     ls $ARCHIVE_PATH
     aws s3 cp $ARCHIVE_PATH s3://2d-public/archives/$ARCHIVE_FILE_NAME
 }
 
-
-
 is_build_cached_locally() {
-    if [ "$EXPECTED_ARCHIVE_CONTENTS" == "$ARCHIVE_CONTENTS" ]; then 
+    if [ "$EXPECTED_ARCHIVE_CONTENTS" == "$ARCHIVE_CONTENTS" ]; then
         return 0
     else
         return 1
