6f75d8b6cfb823825875d16df1ddb49474714d8e
luigi-rosso <hello@rive.app>
2024-01-31 17:28:21 +0000

    Fix GL rendering with URP.
    @HayesGordon this basically boils down to making sure we use our new TextureHelper.Descriptor when generating a RenderTexture to ensure we're using the right internal GraphicsFormat. The RenderTexture takes a high-level format that Unity then tries to map to a compatible native one. We bypass that by explicitly setting which Texture Format we want, which we can tweak based on backend if necessary.
    
    Diffs=
    353cf349b Fix GL rendering with URP. (#6535)
    
    Co-authored-by: Luigi Rosso <luigi-rosso@users.noreply.github.com>

--- skia/dependencies/make_glfw.sh
+++ skia/dependencies/make_glfw.sh
@@ -6,12 +6,11 @@ set -e
 
 GLFW_REPO=https://github.com/glfw/glfw
 
-
 # -----------------------------
 # Get & Build GLFW
 # -----------------------------
 if [ ! -d glfw ]; then
-	echo "Cloning GLFW."
+    echo "Cloning GLFW."
     git clone $GLFW_REPO
 else
     echo "Already have GLFW, update it."
@@ -22,5 +21,5 @@ fi
 mkdir -p glfw_build
 cd glfw_build
 # On Windows, match Skia's /MT flag for link compatibility.
-cmake ../glfw -DBUILD_SHARED_LIBS=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
+cmake ../glfw -DBUILD_SHARED_LIBS=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DGLFW_BUILD_WAYLAND=OFF
 cmake --build . --parallel --config Release
