73684087f03ab7affe9ddf91ec94fe704571e5c5
csmartdalton <hello@rive.app>
2024-05-09 03:28:45 +0000

    Add required descriptor to avoid crash when creating a new RTV
    CreateRenderTargetView doesn't allow a NULL descriptor when the target texture is typeless. This change adds a descriptor (with the assumption that the texture in question is a 2D texture in R8G8B8A8_UNORM format).
    
    Authored-by: Elliot Colp <elliot.colp@gmail.com>
    
    Diffs=
    cd35e613d Add required descriptor to avoid crash when creating a new RTV (#7234)
    
    Co-authored-by: Chris Dalton <99840794+csmartdalton@users.noreply.github.com>
    Co-authored-by: Elliot Colp <elliot.colp@gmail.com>

--- renderer/d3d/pls_render_context_d3d_impl.cpp
+++ renderer/d3d/pls_render_context_d3d_impl.cpp
@@ -716,8 +716,25 @@ ID3D11RenderTargetView* PLSRenderTargetD3D::targetRTV()
 {
     if (m_targetRTV == nullptr && m_targetTexture != nullptr)
     {
+        D3D11_RENDER_TARGET_VIEW_DESC desc{};
+        desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+
+        switch (m_targetFormat)
+        {
+            case DXGI_FORMAT_R8G8B8A8_UNORM:
+            case DXGI_FORMAT_B8G8R8A8_UNORM:
+                desc.Format = m_targetFormat;
+
+            case DXGI_FORMAT_R8G8B8A8_TYPELESS:
+                desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+                break;
+
+            default:
+                RIVE_UNREACHABLE();
+        }
+
         VERIFY_OK(m_gpu->CreateRenderTargetView(m_targetTexture.Get(),
-                                                NULL,
+                                                &desc,
                                                 m_targetRTV.ReleaseAndGetAddressOf()));
     }
     return m_targetRTV.Get();
