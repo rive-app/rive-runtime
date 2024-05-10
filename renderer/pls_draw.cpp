d345355b5b243e1711e8fec9b418fb6f4f6a5199
Elliot Colp <elliot.colp@gmail.com>
2024-05-10 11:31:49 -0600

    Use float4 instead of gvec

--- renderer/pls_draw.cpp
+++ renderer/pls_draw.cpp
@@ -868,7 +868,7 @@ MidpointFanPathDraw::MidpointFanPathDraw(PLSRenderContext* context,
                 // Measure the rotations of curves in batches of 4.
                 assert(j + 4 <= rotationIdx);
 
-                rive::simd::gvec<float, 4> tx0, ty0, tx1, ty1;
+                float4 tx0, ty0, tx1, ty1;
                 std::tie(tx0, ty0, tx1, ty1) = simd::load4x4f(&m_tangentPairs[j][0].x);
 
                 float4 numer = tx0 * tx1 + ty0 * ty1;
