3a647d8b6de1f2b82590f71ac0a39785ee5cf37b
bodymovin <hello@rive.app>
2025-02-07 18:16:59 +0000

    add support for uint properties
    This PR adds support for polygon points.
    This case surfaced a new scenario where uint core properties shouldn't be considered enums but actual uints.
    That requires to treat the source type of certain uint properties different than others.
    In one case, properties will be bindable to enums which internally translate to a finite set of integers.
    On the other one, properties will be bindable to numbers, and we will take care of converting floating point values to unsigned integers under the hood.
    
    Diffs=
    fd830a66ca add support for uint properties (#8982)
    
    Co-authored-by: hernan <hernan@rive.app>

--- src/data_bind/context/context_value_number.cpp
+++ src/data_bind/context/context_value_number.cpp
@@ -1,6 +1,7 @@
 #include "rive/data_bind/context/context_value_number.hpp"
 #include "rive/data_bind/data_values/data_value_number.hpp"
 #include "rive/generated/core_registry.hpp"
+#include <cmath>
 
 using namespace rive;
 
@@ -16,12 +17,35 @@ void DataBindContextValueNumber::apply(Core* target,
     auto value = calculateValue<DataValueNumber, float>(m_dataValue,
                                                         isMainDirection,
                                                         m_dataBind);
-    CoreRegistry::setDouble(target, propertyKey, value);
+    switch (CoreRegistry::propertyFieldId(propertyKey))
+    {
+        case CoreDoubleType::id:
+            CoreRegistry::setDouble(target, propertyKey, value);
+            break;
+        case CoreUintType::id:
+            int rounded = value < 0 ? 0 : std::round(value);
+            CoreRegistry::setUint(target, propertyKey, rounded);
+            break;
+    }
 }
 
 DataValue* DataBindContextValueNumber::getTargetValue(Core* target,
                                                       uint32_t propertyKey)
 {
-    auto value = CoreRegistry::getDouble(target, propertyKey);
-    return new DataValueNumber(value);
+    switch (CoreRegistry::propertyFieldId(propertyKey))
+    {
+        case CoreDoubleType::id:
+        {
+            auto value = CoreRegistry::getDouble(target, propertyKey);
+            return new DataValueNumber(value);
+        }
+        break;
+        case CoreUintType::id:
+        {
+            auto value = (float)CoreRegistry::getUint(target, propertyKey);
+            return new DataValueNumber(value);
+            break;
+        }
+    }
+    return new DataValueNumber(0);
 }
