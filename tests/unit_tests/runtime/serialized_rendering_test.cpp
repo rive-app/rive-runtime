412cad042cf561d2a61345c865914b8acf57c5a3
luigi-rosso <hello@rive.app>
2025-04-02 17:25:08 +0000

    Listener Silver and How to make a Silver
    # Description
    Small example tutorial for how to make a silver, also adds a silver in the process.
    
    # Details
    Details/tutorial here: https://www.notion.so/rive-app/Make-a-new-Silver-1c95da0b06d9808e9fbbe6bdf6219df4?pvs=4
    
    Diffs=
    8bdedb6b19 Listener Silver and How to make a Silver (#9321)
    
    Co-authored-by: Luigi Rosso <luigi-rosso@users.noreply.github.com>

--- tests/unit_tests/runtime/serialized_rendering_test.cpp
+++ tests/unit_tests/runtime/serialized_rendering_test.cpp
@@ -1,6 +1,7 @@
 #include "rive/file.hpp"
 #include "rive/animation/linear_animation.hpp"
 #include "rive/animation/linear_animation_instance.hpp"
+#include "rive/animation/state_machine_instance.hpp"
 #include "utils/serializing_factory.hpp"
 #include "rive_file_reader.hpp"
 #include <catch.hpp>
@@ -51,4 +52,43 @@ TEST_CASE("n-slice silver", "[silver]")
     artboard->draw(renderer.get());
 
     CHECK(silver.matches("n_slice_triangle"));
+}
+
+TEST_CASE("lock icon listener silver", "[silver]")
+{
+    SerializingFactory silver;
+    auto file = ReadRiveFile("assets/lock_icon_demo.riv", &silver);
+
+    auto artboard = file->artboardDefault();
+    silver.frameSize(artboard->width(), artboard->height());
+
+    auto stateMachine = artboard->stateMachineAt(0);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    silver.addFrame();
+
+    // Click in the middle of the state machine.
+    stateMachine->pointerDown(
+        rive::Vec2D(artboard->width() / 2.0f, artboard->height() / 2.0f));
+    // Advance and apply twice to take the transition and apply the next state.
+    stateMachine->advanceAndApply(0.1f);
+    stateMachine->advanceAndApply(1.0f);
+
+    artboard->draw(renderer.get());
+
+    silver.addFrame();
+
+    // Do it again to lock the icon.
+    stateMachine->pointerDown(
+        rive::Vec2D(artboard->width() / 2.0f, artboard->height() / 2.0f));
+    // Advance and apply twice to take the transition and apply the next state.
+    stateMachine->advanceAndApply(0.1f);
+    stateMachine->advanceAndApply(1.0f);
+
+    artboard->draw(renderer.get());
+
+    CHECK(silver.matches("lock_icon_demo"));
 }
