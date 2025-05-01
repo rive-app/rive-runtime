bc95119bfd37b327c3a5922ed7bf7b9041748c52
blakdragan7 <hello@rive.app>
2025-05-01 17:35:51 +0000

    refactor(build scripts) updated setup_windows_dev.bat to act like the ps1 variant (#9577) 7051d2685f updated setup_windows_dev.bat to act like the ps1 variant
    Co-authored-by: Jonathon Copeland <jcopela4@gmail.com>

--- build/build_rive.bat
+++ build/build_rive.bat
@@ -1,12 +1,8 @@
 @echo off
-REM we could check where vs is install via the registery like stated here https://superuser.com/questions/539666/how-to-get-the-visual-studio-installation-path-in-a-batch-file but i think that is overkill
-if exist "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat" (
-    CALL "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
-) else (
-    if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" ( 
-        CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-    ) else echo "Visual Studio 2022 does not appear to be installed, please install visual studio to C:\Program Files\Microsoft Visual Studio"
+REM use fxc because sometimes msbuild is there but fxc is not. The opposite appears to never be true
+WHERE fxc >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    call ..\..\..\scripts\setup_windows_dev.bat
 )
-
 cd %CD%
 sh build_rive.sh %*
--- build/build_rive.ps1
+++ build/build_rive.ps1
@@ -1,10 +1,12 @@
 # requires Set-ExecutionPolicy Bypass -Scope CurrentUser
 $CWD = Get-Location
-if (Get-Command -Name "msbuild" -ErrorAction SilentlyContinue){}
+# use fxc because sometimes msbuild is there but fxc is not. The opposite appears to never be true
+if (Get-Command -Name "fxc" -ErrorAction SilentlyContinue){}
 else
 {
     $Rive_Runtime_Root = Split-Path -Parent $PSScriptRoot
     & $Rive_Runtime_Root\..\..\scripts\setup_windows_dev.ps1
 }
+
 Set-Location $CWD
 sh build_rive.sh $args
