5591fac1b3d767c170df6c7738b038909a5c5e39
aliasbinman <hello@rive.app>
2025-09-16 17:31:47 +0000

    Add profiler Tag macro (#10593) 859d6283c9 Update profiler_macros.h
    Co-authored-by: John White <aliasbinman@gmail.com>

--- include/rive/profiler/profiler_macros.h
+++ include/rive/profiler/profiler_macros.h
@@ -19,10 +19,12 @@
 #define RIVE_PROF_FRAME() OPTICK_FRAME("RiveFrame")
 #define RIVE_PROF_SCOPE() OPTICK_EVENT()
 #define RIVE_PROF_SCOPENAME(name) OPTICK_EVENT(name)
+#define RIVE_PROF_TAG(cat, tag) OPTICK_TAG(cat, tag)
 #define RIVE_PROF_THREAD(name) OPTICK_THREAD(name)
 #else // No profiler selected - fallback to no-op
 #define RIVE_PROF_FRAME()
 #define RIVE_PROF_SCOPE()
 #define RIVE_PROF_SCOPENAME(name)
+#define RIVE_PROF_TAG(cat, tag)
 #define RIVE_PROF_THREAD(name)
 #endif
