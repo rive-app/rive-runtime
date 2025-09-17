ab70997c1a5d96f4643480639f410558f7d388c0
philter <hello@rive.app>
2025-09-17 20:08:43 +0000

    Add silvers for follow path (previously rive_flutter goldens) (#10611) 67f20bfc31 Noticed that the old rive_flutter goldens were removed. With some updates to follow path coming with List follow path, I wanted to make sure those old goldens were captured as silvers prior to the new changes landing. We may want to go back and covert the other goldens in the future.
    Co-authored-by: Philip Chung <philterdesign@gmail.com>

--- tests/unit_tests/runtime/follow_path_constraint_test.cpp
+++ tests/unit_tests/runtime/follow_path_constraint_test.cpp
@@ -1,3 +1,4 @@
+#include "rive/animation/state_machine_instance.hpp"
 #include <rive/file.hpp>
 #include <rive/node.hpp>
 #include <rive/shapes/shape.hpp>
@@ -5,6 +6,7 @@
 #include <utils/no_op_renderer.hpp>
 #include "rive_file_reader.hpp"
 #include "rive_testing.hpp"
+#include "utils/serializing_factory.hpp"
 #include <cstdio>
 
 TEST_CASE("follow path constraint updates world transform", "[file]")
@@ -68,4 +70,85 @@ TEST_CASE(
     auto rectComponents = rectangle->worldTransform().decompose();
     REQUIRE(targetComponents.x() == rectComponents.x());
     REQUIRE(targetComponents.y() == rectComponents.y());
+}
+
+TEST_CASE("Animate shape along follow path", "[silver]")
+{
+    rive::SerializingFactory silver;
+    auto file = ReadRiveFile("assets/follow_path_shapes.riv", &silver);
+
+    auto artboard = file->artboardDefault();
+
+    silver.frameSize(artboard->width(), artboard->height());
+
+    REQUIRE(artboard != nullptr);
+    auto stateMachine = artboard->stateMachineAt(0);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    int frames = 60;
+    for (int i = 0; i < frames; i++)
+    {
+        silver.addFrame();
+        stateMachine->advanceAndApply(0.016f);
+        artboard->draw(renderer.get());
+    }
+
+    CHECK(silver.matches("follow_path_animate_shape"));
+}
+
+TEST_CASE("Animate solo along follow path", "[silver]")
+{
+    rive::SerializingFactory silver;
+    auto file = ReadRiveFile("assets/follow_path_solos.riv", &silver);
+
+    auto artboard = file->artboardDefault();
+
+    silver.frameSize(artboard->width(), artboard->height());
+
+    REQUIRE(artboard != nullptr);
+    auto stateMachine = artboard->stateMachineAt(0);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    int frames = 240;
+    for (int i = 0; i < frames; i++)
+    {
+        silver.addFrame();
+        stateMachine->advanceAndApply(0.016f);
+        artboard->draw(renderer.get());
+    }
+
+    CHECK(silver.matches("follow_path_animate_solo"));
+}
+
+TEST_CASE("Animate follow path target path", "[silver]")
+{
+    rive::SerializingFactory silver;
+    auto file = ReadRiveFile("assets/follow_path_path.riv", &silver);
+
+    auto artboard = file->artboardDefault();
+
+    silver.frameSize(artboard->width(), artboard->height());
+
+    REQUIRE(artboard != nullptr);
+    auto stateMachine = artboard->stateMachineAt(0);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    int frames = 120;
+    for (int i = 0; i < frames; i++)
+    {
+        silver.addFrame();
+        stateMachine->advanceAndApply(0.016f);
+        artboard->draw(renderer.get());
+    }
+
+    CHECK(silver.matches("follow_path_animate_target"));
 }
