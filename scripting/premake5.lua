256cb44fb72675950352314f725501697ee80bef
luigi-rosso <hello@rive.app>
2025-10-24 22:07:27 +0000

    rev to rive_luau_18 (#10901) c1e0f7b595 * rev to rive_luau_18
    * fix: for change in luau
    
    * fix: default old solver
    
    * adding back missing files
    
    * fix: test
    
    * update .riv files
    
    * chore: updated to luau 0.697
    
    Co-authored-by: Luigi Rosso <luigi-rosso@users.noreply.github.com>

--- scripting/premake5.lua
+++ scripting/premake5.lua
@@ -1,5 +1,5 @@
 local dependency = require('dependency')
-local luau = dependency.github('luigi-rosso/luau', 'rive_0_17')
+local luau = dependency.github('luigi-rosso/luau', 'rive_0_19')
 
 dofile('rive_build_config.lua')
 
