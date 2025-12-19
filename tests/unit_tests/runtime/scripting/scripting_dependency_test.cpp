180af1a7fc7cb29535fa5e4f153f0b8bfc788067
philter <hello@rive.app>
2025-12-19 21:40:35 +0000

    Library Scripts export fix (#11294) 978d565911
    fix(editor): Properly remove all script core objects when deleted (#11324) a3e784a1f0
    3 Fixes for Script asset deletion in this PR:
    
    - Present a confirmation dialog if the script is being used by any scripted object on any artboard
    - When script asset is deleted, make sure to clean up all of the associated core code file/line/points
    - Also remove the LibraryCodeFile from the library cache. This fixes a bug where after deleting a library script, you couldn't add it back to the file again.
    
    Co-authored-by: Philip Chung <philterdesign@gmail.com>

--- tests/unit_tests/runtime/scripting/scripting_dependency_test.cpp
+++ tests/unit_tests/runtime/scripting/scripting_dependency_test.cpp
@@ -88,3 +88,98 @@ TEST_CASE("scripted data converter string using multi chain requires",
 
     CHECK(silver.matches("script_converter_with_dependency_2"));
 }
+
+// This test file uses 4 published scripts from a library file with a requires
+// chain StringConverter -> StringPrefix -> StringUppercase -> StringReverse and
+// uses those to convert a string and display it in a text field.
+TEST_CASE(
+    "scripted data converter string using multi chain requires from library",
+    "[silver]")
+{
+    rive::SerializingFactory silver;
+    auto file = ReadRiveFile("assets/script_dependency_test_using_library.riv",
+                             &silver);
+    auto artboard = file->artboardNamed("Artboard");
+
+    silver.frameSize(artboard->width(), artboard->height());
+    REQUIRE(artboard != nullptr);
+    auto stateMachine = artboard->stateMachineAt(0);
+    int viewModelId = artboard.get()->viewModelId();
+
+    auto vmi = viewModelId == -1
+                   ? file->createViewModelInstance(artboard.get())
+                   : file->createViewModelInstance(viewModelId, 0);
+    stateMachine->bindViewModelInstance(vmi);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    rive::ViewModelInstanceString* str =
+        vmi->propertyValue("InputString")->as<rive::ViewModelInstanceString>();
+    REQUIRE(str != nullptr);
+
+    std::vector<std::string> values = {"Hello world!",
+                                       "1,2,3",
+                                       "rive scripting",
+                                       "testing testing testing",
+                                       "Script Data Converter"};
+    for (int i = 0; i < values.size(); i++)
+    {
+        str->propertyValue(values[i]);
+        silver.addFrame();
+        stateMachine->advanceAndApply(0.016f);
+        artboard->draw(renderer.get());
+    }
+
+    CHECK(silver.matches("script_converter_with_dependency_with_library"));
+}
+
+// This test file uses 4 published scripts from the same library file as above
+// EXCEPT that it uses v2 of the Scripts - with a requires chain
+// StringConverter -> StringPrefix -> StringUppercase -> StringReverse and uses
+// those to convert a string and display it in a text field.
+TEST_CASE(
+    "scripted data converter string using multi chain requires from library with update",
+    "[silver]")
+{
+    rive::SerializingFactory silver;
+    auto file =
+        ReadRiveFile("assets/script_dependency_test_using_library_v2.riv",
+                     &silver);
+    auto artboard = file->artboardNamed("Artboard");
+
+    silver.frameSize(artboard->width(), artboard->height());
+    REQUIRE(artboard != nullptr);
+    auto stateMachine = artboard->stateMachineAt(0);
+    int viewModelId = artboard.get()->viewModelId();
+
+    auto vmi = viewModelId == -1
+                   ? file->createViewModelInstance(artboard.get())
+                   : file->createViewModelInstance(viewModelId, 0);
+    stateMachine->bindViewModelInstance(vmi);
+    stateMachine->advanceAndApply(0.1f);
+
+    auto renderer = silver.makeRenderer();
+    artboard->draw(renderer.get());
+
+    rive::ViewModelInstanceString* str =
+        vmi->propertyValue("InputString")->as<rive::ViewModelInstanceString>();
+    REQUIRE(str != nullptr);
+
+    std::vector<std::string> values = {"Hello world!",
+                                       "1,2,3",
+                                       "rive scripting",
+                                       "testing testing testing",
+                                       "Script Data Converter"};
+    for (int i = 0; i < values.size(); i++)
+    {
+        str->propertyValue(values[i]);
+        silver.addFrame();
+        stateMachine->advanceAndApply(0.016f);
+        artboard->draw(renderer.get());
+    }
+
+    CHECK(silver.matches(
+        "script_converter_with_dependency_with_library_with_update"));
+}
