23c275e1dc2183a6d917a95e1917424c1c8eb464
JoshJRive <hello@rive.app>
2026-05-08 07:01:27 +0000

    fix(browserstack): Fix the browserstack run for vk gms (#12473) b70b191146 A recent change updated how arguments were populated and passed in to rive_android_main that required a change in the get_goldens.py script to go with it, this adds that change, plus some missing new gm images.
    Co-authored-by: Josh Jersild <joshua@rive.app>

--- tests/player/player.cpp
+++ tests/player/player.cpp
@@ -498,7 +498,7 @@ int main(int argc, const char* argv[])
     TestingWindow::BackendParams backendParams;
     bool onlyUbershaders = false;
 
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
         if (strcmp(argv[i], "--test_harness") == 0)
         {
